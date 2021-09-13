b788e0b9ba4932e2dfc21d72d78faf6442cfa824
aliev.saftar94@gmail.com <56716472+Saftar94@users.noreply.github.com>
2021-09-13 22:37:05 +0300

    test 2
    test 2

--- JS DZ-7-modul/js/task-2.js
+++ JS DZ-7-modul/js/task-2.js
@@ -6,8 +6,19 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ]
-const elemUl = document.querySelector('#endregion')
-const addLi = ingredients.reduce((str, value) => str + `<li>${value}</li> `, '')
-console.log(addLi)
+// const elemUl = document.querySelector('#endregion')
+// const addLi = ingredients.reduce((str, value) => str + `<li>${value}</li> `, '')
+// console.log(addLi)
 
-elemUl.innerHTML = addLi
+// elemUl.innerHTML = addLi
+
+let elemUl = document.querySelector('#ingredients')
+
+const addLi = ingredients.map((ingredient) => {
+  const itemLi = document.createElement('li')
+  itemLi.textContent = ingredient
+  console.log(ingredient)
+  return addLi
+})
+
+elemUl.append(...addLi)
